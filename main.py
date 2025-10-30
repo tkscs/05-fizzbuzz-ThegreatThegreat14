7bc8c63f0aa9dc07d19fefedf5fc4303c87d3a4b
ThegreatThegreat14 <amaydan2028@kehillahstudent.org>
2025-10-30 09:26:06 -0700

    PICK YOUR OWN ADVENTURE

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ if the number is a multiple of both 3 and 5, print "fizzbuzz" instead of the num
 
 
 Maximum_Number = 15
+Three_Only = "Fizz"
+Five_Only = "Buzz"
+Three_And_Five = "Fizzbuzz"
 
 for Number in range (1, Maximum_Number + 1):
     if Number % 3 == 0 and Number % 5 == 0:
-        print ("Fizzbuzz")
+        print (Three_And_Five)
     elif Number % 3 == 0:
-        print ("Fizz")
+        print (Three_Only)
     elif Number % 5 == 0:
-        print ("Buzz")
+        print (Five_Only)
     if Number % 3 != 0 and Number % 5 != 0:
         print (Number)
